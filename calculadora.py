a25975ebd8f7264a9ee0dfa9c465990a523109ef
Dimas de Abreu <dimasabreu@live.com>
2022-08-22 09:53:00 -0300

    Codigo melhorado

--- calculadora.py
+++ calculadora.py
@@ -1,150 +1,136 @@
 import pyttsx3
 import speech_recognition as sr
-import contas
+from contas import *
 
+# Parte de inicializacao
+
+print("-"*40)
+print("Bem-vindo a Calculadora falante!\n", end="")
+print("ESPERE SEMPRE UMA CONFIRMAÇÃO VISUAL\n", end="")
+print("Para comecar diga: Abrir calculadora")
 r = sr.Recognizer()
 with sr.Microphone() as source:
-    print("""Bem-vindo a Calculadora falante!
-(espere sempre uma confirmação visual)""")
-    print("-"*40)
-    engine = pyttsx3.init()
-    engine.say("Fale o que você gostaria de fazer")
-    engine.runAndWait()
-    print("Fale o que voce gostaria de fazer:")
     audio = r.listen(source)
-    opcao = r.recognize_google(audio, language="pt-BR")
-    print("Voce escolheu", opcao.upper(), "!")
-    if opcao == "somar":
+    ini = r.recognize_google(audio, language="pt-BR")
+    if ini == "Abrir calculadora":
+        print("Calculadora iniciada!")
         engine = pyttsx3.init()
-        engine.say("Fale o primeiro numero")
+        engine.say("Bem vindo, vou listar abaixo as opcões disponiveis:")
         engine.runAndWait()
-        print("Fale o primeiro numero:")
+        print("OPÇÕES:\nSomar\nSubtrair\nMultiplicar\nDividir", end="")
+        print("\nPorcentagem\nExponenciação")
+        engine = pyttsx3.init()
+        engine.say("Fale o que você gostaria de fazer")
+        engine.runAndWait()
+        print("Fale o que voce gostaria de fazer:")
         audio = r.listen(source)
-        numr1 = r.recognize_google(audio, language="pt-BR")
-        print("O numero escolhido foi:", numr1)
-        engine = pyttsx3.init()
-        engine.say("Fale o segundo numero")
-        engine.runAndWait()
-        print("Fale o segundo numero:")
-        audio = r.listen(source)
-        numr2 = r.recognize_google(audio, language="pt-BR")
-        print("O numero escolhido foi:", numr2)
-        num1 = float(numr1)
-        num2 = float(numr2)
-        print(contas.somar(num1, num2))
-        engine = pyttsx3.init()
-        engine.say(contas.somar(num1, num2))
-        engine.runAndWait()
-        print("-"*40)
-    elif opcao == "subtrair":
-        engine = pyttsx3.init()
-        engine.say("Fale o primeiro numero")
-        engine.runAndWait()
-        print("Fale o primeiro numero:")
-        audio = r.listen(source)
-        numr1 = r.recognize_google(audio, language="pt-BR")
-        print("O numero escolhido foi:", numr1)
-        engine = pyttsx3.init()
-        engine.say("Fale o segundo numero")
-        engine.runAndWait()
-        print("Fale o segundo numero:")
-        audio = r.listen(source)
-        numr2 = r.recognize_google(audio, language="pt-BR")
-        print("O numero escolhido foi:", numr2)
-        num1 = float(numr1)
-        num2 = float(numr2)
-        print(contas.subtrair(num1, num2))
-        engine = pyttsx3.init()
-        engine.say(contas.subtrair(num1, num2))
-        engine.runAndWait()
-        print("-"*40)
-    elif opcao == "multiplicar":
-        engine = pyttsx3.init()
-        engine.say("Fale o primeiro numero")
-        engine.runAndWait()
-        print("Fale o primeiro numero:")
-        audio = r.listen(source)
-        numr1 = r.recognize_google(audio, language="pt-BR")
-        print("O numero escolhido foi:", numr1)
-        engine = pyttsx3.init()
-        engine.say("Fale o segundo numero")
-        engine.runAndWait()
-        print("Fale o segundo numero:")
-        audio = r.listen(source)
-        numr2 = r.recognize_google(audio, language="pt-BR")
-        print("O numero escolhido foi:", numr2)
-        num1 = float(numr1)
-        num2 = float(numr2)
-        print(contas.multiplicar(num1, num2))
-        engine = pyttsx3.init()
-        engine.say(contas.multiplicar(num1, num2))
-        engine.runAndWait()
-        print("-"*40)
-    elif opcao == "dividir":
-        engine = pyttsx3.init()
-        engine.say("Fale o primeiro numero")
-        engine.runAndWait()
-        print("Fale o primeiro numero:")
-        audio = r.listen(source)
-        numr1 = r.recognize_google(audio, language="pt-BR")
-        print("O numero escolhido foi:", numr1)
-        engine = pyttsx3.init()
-        engine.say("Fale o segundo numero")
-        engine.runAndWait()
-        print("Fale o segundo numero:")
-        audio = r.listen(source)
-        numr2 = r.recognize_google(audio, language="pt-BR")
-        print("O numero escolhido foi:", numr2)
-        num1 = float(numr1)
-        num2 = float(numr2)
-        print(contas.dividir(num1, num2))
-        engine = pyttsx3.init()
-        engine.say(contas.dividir(num1, num2))
-        engine.runAndWait()
-        print("-"*40)
-    elif opcao == "porcentagem":
-        engine = pyttsx3.init()
-        engine.say("Fale o primeiro numero")
-        engine.runAndWait()
-        print("Fale o primeiro numero:")
-        audio = r.listen(source)
-        numr1 = r.recognize_google(audio, language="pt-BR")
-        print("O numero escolhido foi:", numr1)
-        engine = pyttsx3.init()
-        engine.say("Fale o segundo numero")
-        engine.runAndWait()
-        print("Fale o segundo numero:")
-        audio = r.listen(source)
-        numr2 = r.recognize_google(audio, language="pt-BR")
-        print("O numero escolhido foi:", numr2)
-        num1 = float(numr1)
-        num2 = float(numr2)
-        print(contas.porcentagem(num1, num2))
-        engine = pyttsx3.init()
-        engine.say(contas.porcentagem(num1, num2))
-        engine.runAndWait()
-        print("-"*40)
-    elif opcao == "exponenciação":
-        engine = pyttsx3.init()
-        engine.say("Fale o primeiro numero")
-        engine.runAndWait()
-        print("Fale o primeiro numero:")
-        audio = r.listen(source)
-        numr1 = r.recognize_google(audio, language="pt-BR")
-        print("O numero escolhido foi:", numr1)
-        engine = pyttsx3.init()
-        engine.say("Fale o segundo numero")
-        engine.runAndWait()
-        print("Fale o segundo numero:")
-        audio = r.listen(source)
-        numr2 = r.recognize_google(audio, language="pt-BR")
-        print("O numero escolhido foi:", numr2)
-        num1 = float(numr1)
-        num2 = float(numr2)
-        print(contas.exponenciacao(num1, num2))
-        engine = pyttsx3.init()
-        engine.say(contas.exponenciacao(num1, num2))
-        engine.runAndWait()
-        print("-"*40)
+        opcao = r.recognize_google(audio, language="pt-BR")
+        if opcao == "Fechar":
+            exit()
+        else:
+            print("Voce escolheu", opcao.upper(), "!")
     else:
+        engine = pyttsx3.init()
+        engine.say("Não entendi")
+        engine.runAndWait()
         exit()
+print("-"*40)
+
+
+"""r = sr.Recognizer()
+with sr.Microphone() as source:
+    audio = r.listen(source)
+    opcao = r.recognize_google(audio, language="pt-BR")"""
+
+
+# func para captar audio
+
+
+def captacao():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        engine = pyttsx3.init()
+        engine.say("Fale o numero")
+        engine.runAndWait()
+        print("Fale o numero:")
+        audio = r.listen(source)
+        num = r.recognize_google(audio, language="pt-BR")
+        return (num)
+
+
+# func para falar o resultado
+
+
+def fala():
+    engine = pyttsx3.init()
+    engine.say(falando)
+    engine.runAndWait()
+
+
+# comecando o programa
+
+if opcao == "somar":
+    num1 = float(captacao())
+    print("O numero falado foi:", num1)
+    num2 = float(captacao())
+    print("O numero falado foi:", num2)
+    print(somar(num1, num2))
+    falando = (somar(num1, num2))
+    fala()
+    print("-"*40)
+
+elif opcao == "subtrair":
+    num1 = float(captacao())
+    print("O numero falado foi:", num1)
+    num2 = float(captacao())
+    print("O numero falado foi:", num2)
+    print(subtrair(num1, num2))
+    falando = (subtrair(num1, num2))
+    fala()
+    print("-"*40)
+
+elif opcao == "multiplicar":
+    num1 = float(captacao())
+    print("O numero falado foi:", num1)
+    num2 = float(captacao())
+    print("O numero falado foi:", num2)
+    print(multiplicar(num1, num2))
+    falando = (multiplicar(num1, num2))
+    fala()
+    print("-"*40)
+
+elif opcao == "dividir":
+    num1 = float(captacao())
+    print("O numero falado foi:", num1)
+    num2 = float(captacao())
+    print("O numero falado foi:", num2)
+    print(dividir(num1, num2))
+    falando = (dividir(num1, num2))
+    fala()
+    print("-"*40)
+
+elif opcao == "porcentagem":
+    num1 = float(captacao())
+    print("O numero falado foi:", num1)
+    num2 = float(captacao())
+    print("O numero falado foi:", num2)
+    print(porcentagem(num1, num2))
+    falando = (porcentagem(num1, num2))
+    fala()
+    print("-"*40)
+
+elif opcao == "exponenciação":
+    num1 = float(captacao())
+    print("O numero falado foi:", num1)
+    num2 = float(captacao())
+    print("O numero falado foi:", num2)
+    print(exponenciacao(num1, num2))
+    falando = (exponenciacao(num1, num2))
+    fala()
+    print("-"*40)
+
+else:
+    falando = "não entendi"
+    fala()
+    exit()
+
--- contas.py
+++ contas.py
@@ -20,13 +20,13 @@ def multiplicar(num1, num2):
 
 def dividir(num1, num2):
     div = num1 / num2
-    resultado = (f"A divisão dos numeros é {div}")
+    resultado = ("A divisão dos numeros é %.2f" % (div))
     return resultado
 
 
 def porcentagem(num1, num2):
     por = (num1 / 100) * num2
-    resultado = (f"{num1} porcento de {num2} é {por}")
+    resultado = ("%d porcento de %d é %.2f" % (num1, num2, por))
     return resultado
 
 
@@ -34,3 +34,5 @@ def exponenciacao(num1, num2):
     expo = num1 ** num2
     resultado = (f"{num1} elevado a {num2} é {expo}")
     return resultado
+
+#def primo():
